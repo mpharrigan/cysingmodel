f5dfce29d157329eb6a5692d30c3cf0aefd8591e
Matthew Harrigan <harrigan@stanford.edu>
2014-07-08 20:54:56 -0700

    Performance is good. Tunable H, J, Temp

--- cysingmodel/_ising.pyx
+++ cysingmodel/_ising.pyx
@@ -15,26 +15,17 @@ from cpython cimport bool
 # Constants
 cdef int NDIM = 2
 cdef int BOXL = 100
-cdef int J = 20
-cdef int H = 10
-cdef float BETA = 1 / 30
 
 DTYPEI1 = np.int8
 DTYPEI = np.int
+DTYPED = np.double
 
 ctypedef np.int_t DTYPEI_T
 ctypedef np.int8_t DTYPEI1_T
-
-# Neighbor offsets
-cdef np.ndarray NEIGHB = np.asarray([
-    [-1, 0],
-    [0, -1],
-    [1, 0],
-    [0, 1]
-], dtype=DTYPEI)
+ctypedef np.float DTYPED_T
 
 
-cdef np.ndarray[DTYPEI_T, ndim=2] generate_hmask(int y):
+cdef np.ndarray[DTYPEI_T, ndim=2] generate_hmask(int y, int H):
     """Generate single-body field, H.
 
     This will be positive for the whole space except for two blocks
@@ -82,60 +73,81 @@ def _block(int x, int y, int size=20):
     return slice(x - s2, x + s2), slice(y - s2, y + s2)
 
 
-def mc_loop(int n_steps, np.ndarray[DTYPEI1_T, ndim=2] cells, int stride=1000, bool equilib=False):
+def mc_loop(int n_steps, np.ndarray[DTYPEI1_T, ndim=2] cells,
+        int stride=1000, bool equilib=False,
+        int J=20, int H=10, int TEMP=30):
     """Perform Monte Carlo simulation."""
 
     # Generate H
-    cdef np.ndarray[DTYPEI_T, ndim=2] hmask = generate_hmask(0)
+    cdef np.ndarray[DTYPEI_T, ndim=2] hmask = generate_hmask(0, H=H)
 
     # Save configurations
     cdef np.ndarray[DTYPEI1_T, ndim=3] cells_t = np.ones((n_steps // stride, BOXL, BOXL), dtype=DTYPEI1)
     cells_t[0, :, :] = cells
 
     # Magnetization
-    cdef np.ndarray[DTYPEI_T, ndim=1] m = np.zeros(n_steps, dtype=DTYPEI)
+    cdef np.ndarray[DTYPEI_T, ndim=1] m = np.zeros(n_steps + 1, dtype=DTYPEI)
+    m[0] = np.sum(cells)
 
     cdef int s_pick
-    cdef np.ndarray[DTYPEI_T, ndim=1] pick_i
-    cdef np.ndarray[DTYPEI_T, ndim=2] neighbs
+    cdef int picki1, picki2
     cdef np.ndarray[DTYPEI1_T, ndim=1] cell_neighbs
 
     cdef int e_old, e_new
     cdef float prob
     cdef int step
 
+    # Make all the random integers
+    cdef np.ndarray[DTYPEI_T, ndim=1] randints = np.random.random_integers(0, BOXL - 1, NDIM * n_steps)
+    # And floats
+    cdef np.ndarray[double, ndim=1] randfloats = np.random.random_sample(n_steps)
+
     for step in range(n_steps):
 
         # Pick a cell
-        pick_i = np.random.random_integers(0, BOXL - 1, NDIM)
-        s_pick = cells[pick_i[0], pick_i[1]]
+        picki1 = randints[step]
+        picki2 = randints[n_steps + step]
+        s_pick = cells[picki1, picki2]
 
-        # Find neighbors
-        neighbs = np.remainder(pick_i + NEIGHB, BOXL)
-        cell_neighbs = cells[neighbs[:, 0], neighbs[:, 1]]
+        cell_neighbs = np.array([
+                cells[(picki1 + 1) % BOXL, picki2],
+                cells[picki1, (picki2 + 1) % BOXL],
+                cells[(picki1 - 1) % BOXL, picki2],
+                cells[picki1, (picki2 - 1) % BOXL]
+        ], dtype=DTYPEI1)
 
         # Calculate the old energy
-        e_old = -J * np.sum(s_pick * cell_neighbs) - hmask[
-            pick_i[0], pick_i[1]] * s_pick
+        e_old = -J * (
+                s_pick * cell_neighbs[0] +
+                s_pick * cell_neighbs[1] +
+                s_pick * cell_neighbs[2] +
+                s_pick * cell_neighbs[3]
+                ) - hmask[picki1, picki2] * s_pick
 
         # Flip the spin and see what the new energy would be
         s_pick *= -1
-        e_new = -J * np.sum(s_pick * cell_neighbs) - hmask[
-            pick_i[0], pick_i[1]] * s_pick
+        e_new = -J * (
+                s_pick * cell_neighbs[0] +
+                s_pick * cell_neighbs[1] +
+                s_pick * cell_neighbs[2] +
+                s_pick * cell_neighbs[3]
+                ) - hmask[picki1, picki2] * s_pick
 
         # Accept with probability
-        prob = np.exp(-BETA * (e_new - e_old))
-        if prob > np.random.ranf():
-            cells[pick_i[0], pick_i[1]] *= -1
+        prob = np.exp(-(e_new - e_old)/TEMP)
+        if prob > randfloats[step]:
+            cells[picki1, picki2] *= -1
+
+            # Magnetization changes by two times the new value
+            m[step + 1] = m[step] + 2 * s_pick
+        else:
+            m[step + 1] = m[step]
 
         # Save configuration
         if step % stride == 0:
             cells_t[step // stride, :, :] = cells
             if not equilib:
-                hmask = generate_hmask(step // stride)
-
-        # Compute total magnetization
-        m[step] = np.sum(cells)
+                hmask = generate_hmask(step // stride, H=H)
 
     return cells_t, m
 
