83fc2eb31feed5b7e1e8d703dcb39d5e7f5dc7cd
Matthew Harrigan <harrigan@stanford.edu>
2014-07-10 20:17:51 -0700

    Stupid initial condition

--- cysingmodel/_ising.pyx
+++ cysingmodel/_ising.pyx
@@ -48,8 +48,8 @@ def generate_cells():
     """
     cdef np.ndarray[DTYPEI1_T, ndim=2] cells = np.ones((BOXL, BOXL), dtype=DTYPEI1)
 
-    cells[_block(35 - 20, 50)] = -1
-    cells[_block(85 + 20, 50)] = -1
+    cells[_block(35 - 15, 50)] = -1
+    cells[_block(85 + 15, 50)] = -1
 
     return cells
 
